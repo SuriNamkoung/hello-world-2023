1736b244b450ef398999c49b923547fd70788ee3
SuriNamkoung <ynamkoung@sva.edu>
2021-10-06 13:04:03 -0400

    Create Suri(Yeseul Namkoung)_HW4.py

---
+++ homework/Suri(Yeseul Namkoung)_HW4.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+
+angle = 89           
+t.bgcolor("black")   
+t.color("green")     
+t.speed(10)           
+for x in range(200): 
+    t.forward(x)    
+    t.left(angle)   
+
+t.hideturtle();      
+
